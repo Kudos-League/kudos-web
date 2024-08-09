b4c6ec1e22cf94f1bbe7feb80fb4294db15da23d
Jordy Greenblatt <jordan.s.greenblatt@gmail.com>
2024-08-09 06:28:55 -0400

    Add tab icons

--- home/home.tsx
+++ home/home.tsx
@@ -1,15 +1,21 @@
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { BottomTabNavigationOptions, createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome from '@expo/vector-icons/FontAwesome';
 
 import Feed from './tabs/feed';
 import Notifications from './tabs/notifications';
+import { ComponentProps } from 'react';
 
 const Tab = createBottomTabNavigator();
 
 export default function Home() {
   return (
     <Tab.Navigator screenOptions={{ headerShown: false }}>
-      <Tab.Screen name="Feed" component={Feed} />
-      <Tab.Screen name="Notifications" component={Notifications} />
+      <Tab.Screen name="Feed" options={getTabOptions('history')} component={Feed} />
+      <Tab.Screen name="Notifications" options={getTabOptions('inbox')} component={Notifications} />
     </Tab.Navigator>
   );
+}
+
+function getTabOptions(iconName: ComponentProps<typeof FontAwesome>['name']): BottomTabNavigationOptions {
+  return { tabBarIcon: ({color}) => <FontAwesome size={20} style={{ marginBottom: -3 }} name={iconName} color={color} /> };
 }
