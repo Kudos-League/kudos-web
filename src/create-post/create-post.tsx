c4afaa6426e8f77bc23628608be7a20841fc295e
Jordy Greenblatt <jordan.s.greenblatt@gmail.com>
2024-08-15 16:08:30 -0400

    Add radio button providing isRequest bit for createPost

--- src/create-post/create-post.tsx
+++ src/create-post/create-post.tsx
@@ -1,4 +1,4 @@
-import { View } from "react-native";
+import { View, Text } from "react-native";
 
 import { SubmitHandler, useForm } from 'react-hook-form';
 
@@ -6,17 +6,35 @@ import { Button } from "react-native-paper";
 
 import globalStyles from "shared/styles";
 import Input from "shared/components/forms/input";
+import RadioGroup from "shared/components/forms/radio-group";
 
 export default function CreatePost() {
   const { handleSubmit, control } = useForm<FormValues>();
 
   const onSubmit: SubmitHandler<FormValues> = (data) => {
-    console.log(`submitted form with data:\n${JSON.stringify(data, null, 2)}`);
-    alert(`submitted form with data:\n${JSON.stringify(data, null, 2)}`);
+    const request = {
+      title: data.title,
+      body: data.body,
+      isRequest: data.type === 'request',
+    }
+    console.log(`submitted form for request:\n${JSON.stringify(request, null, 2)}`);
+    alert(`submitted form with request:\n${JSON.stringify(request, null, 2)}`);
   }
 
   return (
     <View style={globalStyles.container}>
+      <View style={globalStyles.formRow}>
+        <Text>Is this post</Text>
+        <RadioGroup
+          name="type"
+          control={control}
+          options={[
+            { label: 'in search of', value: 'request' },
+            { label: 'offering up', value: 'offer' },
+          ]}
+        />
+        <Text>these item(s)?</Text>
+      </View>
       <View style={globalStyles.formRow}>
         <Input name="title" label="Title" control={control} />
       </View>
@@ -33,5 +51,5 @@ export default function CreatePost() {
 type FormValues = {
   title: string;
   body: string;
-  isRequest: boolean;
+  type: 'request'|'offer';
 }
--- src/shared/components/forms/input.tsx
+++ src/shared/components/forms/input.tsx
@@ -6,7 +6,7 @@ type Props<T extends FieldValues> = {
     name: string;
     label: string;
     control: Control<T>;
-} & UseControllerProps<T>
+} & UseControllerProps<T>;
 
 export default function Input<T extends FieldValues>({name, control, label}: Props<T>) {
     const { field } = useController<T>({
