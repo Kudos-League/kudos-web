6bb8f28e36ca568e56d9f24866f4eb24f2c2ed41
Jordy Greenblatt <jordan.s.greenblatt@gmail.com>
2024-08-29 12:54:25 -0400

    Switch to an Android-friendly picker component

--- src/shared/components/forms/picker.tsx
+++ src/shared/components/forms/picker.tsx
@@ -1,4 +1,6 @@
-import { Picker as NativePicker } from "@react-native-picker/picker";
+import { useState } from 'react';
+import { StyleSheet } from 'react-native';
+import { Dropdown } from 'react-native-element-dropdown';
 import { useController, Control, FieldValues, UseControllerProps, UseFormReturn } from 'react-hook-form';
 
 type Props<T extends FieldValues> = {
@@ -15,10 +17,46 @@ export default function Picker<T extends FieldValues>({ name, options, form }: P
   });
 
   return (
-      <NativePicker
-          selectedValue={field.value}
-          onValueChange={field.onChange}>
-        {options.map((option) => <NativePicker.Item label={option.label} value={option.value} /> )}
-      </NativePicker>
+    <Dropdown
+      style={styles.dropdown}
+      placeholderStyle={styles.placeholderStyle}
+      selectedTextStyle={styles.selectedTextStyle}
+      data={options}
+      maxHeight={300}
+      labelField="label"
+      valueField="value"
+      value={field.value}
+      onChange={({value}) => {
+        field.onChange({target: {value}});
+      }}
+    />
   );
-}
+};
+
+
+const styles = StyleSheet.create({
+  dropdown: {
+    margin: 16,
+    height: 50,
+    borderBottomColor: 'gray',
+    borderBottomWidth: 0.5,
+    flex: 1,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
