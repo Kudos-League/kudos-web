552bec8dab8dffd6329585163ae7c06b7381742f
Jordy Greenblatt <jordan.s.greenblatt@gmail.com>
2024-09-25 14:43:51 -0400

    Add dev server with hardcoded global environment

--- src/shared/api/config.ts
+++ src/shared/api/config.ts
@@ -1,12 +1,17 @@
 export enum Environment {
   LOCAL,
+  DEV,
 }
 
-// TODO: Set the environment as a global constant and remove the argument
-export function getEndpointUrl(env: Environment): string {
+// TODO: Read this from a flag or environment variable
+const env: Environment = Environment.LOCAL;
+
+export function getEndpointUrl(): string {
   switch (env) {
     case Environment.LOCAL:
       return 'http://localhost:3005';
+      case Environment.DEV:
+        return 'http://67.207.81.174:3005';
     default:
       throw new Error(`No endpoint specified for environment ${env}`);
   }
