9148308a7406980b55a6110ce300684318dbddd2
Jordy Greenblatt <jordan.s.greenblatt@gmail.com>
2024-10-29 16:52:47 -0400

    Update donate page TODO to include Stripe RN SDK

--- src/pages/donate/donate.tsx
+++ src/pages/donate/donate.tsx
@@ -5,7 +5,10 @@ import globalStyles from "shared/styles";
 export default function Feed() {
     return (
         <View style={globalStyles.container}>
-            <Text>TODO: Add donate page (use Stripe template from pre-ReactNative version).</Text>
+            <Text>
+              TODO: Add donate page (based on Stripe integration from pre-ReactNative version).
+              See the Stripe React native widget doc: https://docs.stripe.com/sdks/react-native.
+            </Text>
         </View>
     )
 }
