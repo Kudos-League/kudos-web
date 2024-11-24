be3e6c1282b7920fedcccb22dd0ebfd5d386b6a1
Isabella <isabellafj97@gmail.com>
2024-11-23 21:59:21 -0600

    fix createPost

--- src/shared/api/actions.ts
+++ src/shared/api/actions.ts
@@ -1,29 +1,59 @@
 import { getEndpointUrl } from "./config";
-import { CreatePostDTO, CreateUserDTO, PostDTO, UserLoginRequestSchemaDTO, UserLoginResponseDTO } from "./types";
+import {
+  CreatePostDTO,
+  CreateUserDTO,
+  PostDTO,
+  UserLoginRequestSchemaDTO,
+  UserLoginResponseDTO,
+} from "./types";
 import axios from "axios";
+
+/**
+ * Converts a DTO object into a FormData object.
+ * Supports nested fields and array data types.
+ */
+function toFormData(dto: Record<string, any>): FormData {
+  const formData = new FormData();
+
+  for (const key in dto) {
+    const value = dto[key];
+
+    if (Array.isArray(value)) {
+      value.forEach((v) => formData.append(`${key}[]`, v));
+    } else {
+      formData.append(key, value);
+    }
+  }
+
+  return formData;
+}
 
 // TODO: Type all request/response values if/when we factor API types into a codebase shared by FE/BE
 
 /** @throws {AxiosError} */
 export async function createPost(request: CreatePostDTO, token: string) {
-  return await axios.post(
-    `${getEndpointUrl()}/posts`,
-    request,
-    {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': `Bearer ${token}`
-      }},
-  );
+  const formData = toFormData(request);
+  const response = await axios.post(`${getEndpointUrl()}/posts`, formData, {
+    headers: {
+      "Content-Type": "multipart/form-data",
+      Authorization: `Bearer ${token}`,
+    },
+  });
+
+  return response.data;
 }
 
 /** @throws {AxiosError} */
-export async function register(request: CreateUserDTO): Promise<{data: UserLoginResponseDTO}> {
+export async function register(
+  request: CreateUserDTO
+): Promise<{ data: UserLoginResponseDTO }> {
   return await axios.post(`${getEndpointUrl()}/users/register`, request);
 }
 
 /** @throws {AxiosError} */
-export async function login(request: UserLoginRequestSchemaDTO): Promise<{data: UserLoginResponseDTO}> {
+export async function login(
+  request: UserLoginRequestSchemaDTO
+): Promise<{ data: UserLoginResponseDTO }> {
   return await axios.post(`${getEndpointUrl()}/users/login`, request);
 }
 
